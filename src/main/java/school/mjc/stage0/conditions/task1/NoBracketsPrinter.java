79168d1e159d3773b3603e5ecc76f357dab9b3fb
litim smail <litimsmail@gmail.com>
2024-10-15 15:39:09 +0100

    IsPositive

--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -4,7 +4,7 @@ public class NoBracketsPrinter {
     public static void main(String[] args) {
         if(false){ 
             System.out.println("The statement is true");
-        System.out.println("I'm out of checking");
+            System.out.println("I'm out of checking");
         }
     }
 }
